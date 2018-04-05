27f7dc5bfe7c3271e9aa22f0dc9de0a7d196078b
Monroejack <Monroejack2121@gmail.com>
2018-04-05 09:27:55 -0400

    Added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users
+  root 'index#users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
