621e8936ab68cdc895530499e3723968547e2346
Monroejack <Monroejack2121@gmail.com>
2018-04-05 09:42:28 -0400

    more changes

--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :name, :user_id, :created_at, :updated_at
+json.extract! user, :id, :name, :team_id, :created_at, :updated_at
 json.url user_url(user, format: :json)
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { name: @user.name, user_id: @user.user_id } }
+      post users_url, params: { user: { name: @user.name, team_id: @user.team_id } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,7 +34,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { name: @user.name, user_id: @user.user_id } }
+    patch user_url(@user), params: { user: { name: @user.name, team_id: @user.team_id } }
     assert_redirected_to user_url(@user)
   end
 
